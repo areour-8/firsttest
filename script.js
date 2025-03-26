441eb9a624e002f31ab58a8335b6fe8e63647b9e
areour-8 <hackerlran45@gmail.com>
2025-03-26 17:09:00 +0330

    Delete script.js
    Signed-off-by: areour-8 <hackerlran45@gmail.com>

--- script.js
+++
@@ -1,11 +0,0 @@
-// تابع نمایش پیام
-function showAlert() {
-    alert('سلام! خوشحالم که از سایت من دیدن میکنید 😊');
-}
-
-// تغییر متن فوتر هنگام بارگذاری صفحه
-document.addEventListener('DOMContentLoaded', function() {
-    const footer = document.querySelector('footer p');
-    const year = new Date().getFullYear();
-    footer.textContent = `© ${year} ساختهشده با عشق ❤️`;
-});
